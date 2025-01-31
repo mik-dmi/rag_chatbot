40a309aee56ff0c0f90ff4665834cdf08c2cba4c
mike_dev <122227240+mik-dmi@users.noreply.github.com>
2025-01-31 01:39:42 +0000

    Added some basic middlewares

--- backend/cmd/api/api.go
+++ backend/cmd/api/api.go
@@ -2,8 +2,12 @@ package main
 
 import (
 	"fmt"
+	"log/slog"
 	"net/http"
+	"os"
 	"time"
+
+	middleware "github.com/mik-dmi/rag_chatbot/backend/utils"
 )
 
 type application struct {
@@ -16,22 +20,44 @@ type config struct {
 func (app *application) mount() *http.ServeMux {
 	mux := http.NewServeMux()
 
-	mux.HandleFunc(" /v1/ragchatbot", app.healthCheckHandler)
-	return mux
+	mux.HandleFunc("POST /query", app.healthCheckHandler)
+	mux.HandleFunc("POST /vector-db", app.healthCheckHandler)
+
+	v1 := http.NewServeMux()
+	v1.Handle("/v1/", http.StripPrefix("/v1", mux)) //dealing with subrouting
+	return v1
 
 }
 
 func (app *application) Run(mux *http.ServeMux) error {
 
+	stack := middleware.CreateStack(
+		middleware.Timeout(90*time.Second),
+		middleware.Recovery, // Added first (but runs last)
+		middleware.Logging,
+	)
+
 	svr := &http.Server{
 		Addr:         app.config.addr,
-		Handler:      mux,
+		Handler:      stack(mux),
 		WriteTimeout: time.Second * 30,
 		ReadTimeout:  time.Second * 10,
 		IdleTimeout:  time.Minute,
 	}
 
-	fmt.Printf("server is running at %s", app.config.addr)
+	fmt.Println("server is running at", app.config.addr)
 	return svr.ListenAndServe()
 
+}
+
+func LoggingMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
+		logger.Info("Request Info",
+			slog.String("method", r.Method),
+			slog.String("path", r.RequestURI),
+			slog.String("host", r.Host),
+		)
+		next.ServeHTTP(w, r)
+	})
 }
---
+++ backend/utils/middleware.go
@@ -0,0 +1,88 @@
+package middleware
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"time"
+)
+
+type wrappedWriter struct {
+	http.ResponseWriter
+	statusCode int
+}
+
+func (w *wrappedWriter) WriteHeader(statusCode int) {
+	w.ResponseWriter.WriteHeader(statusCode)
+	w.statusCode = statusCode
+}
+
+func Logging(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		start := time.Now()
+
+		wrapped := &wrappedWriter{
+			ResponseWriter: w,
+			statusCode:     http.StatusOK,
+		}
+		next.ServeHTTP(wrapped, r)
+		log.Println(wrapped.statusCode, r.Method, r.URL.Path, time.Since(start))
+	})
+}
+
+func Recovery(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+
+		defer func() {
+			err := recover()
+			if err != nil {
+				fmt.Println(err)
+
+				jsonBody, _ := json.Marshal(map[string]string{
+					"error": "There was an internal server error",
+				})
+
+				w.Header().Set("Content-Type", "application/json")
+				w.WriteHeader(http.StatusInternalServerError)
+				w.Write(jsonBody)
+			}
+
+		}()
+
+		next.ServeHTTP(w, r)
+
+	})
+}
+
+type Middleware func(http.Handler) http.Handler
+
+// chain middleware
+func CreateStack(xs ...Middleware) Middleware {
+	return func(next http.Handler) http.Handler {
+		for i := len(xs) - 1; i >= 0; i-- {
+			x := xs[i]
+			next = x(next)
+		}
+		return next
+	}
+}
+
+func Timeout(timeout time.Duration) func(next http.Handler) http.Handler {
+	return func(next http.Handler) http.Handler {
+		fn := func(w http.ResponseWriter, r *http.Request) {
+			ctx, cancel := context.WithTimeout(r.Context(), timeout)
+			defer func() {
+				cancel()
+				if ctx.Err() == context.DeadlineExceeded {
+					w.WriteHeader(http.StatusGatewayTimeout)
+				}
+			}()
+
+			r = r.WithContext(ctx)
+			next.ServeHTTP(w, r)
+		}
+		return http.HandlerFunc(fn)
+	}
+}
